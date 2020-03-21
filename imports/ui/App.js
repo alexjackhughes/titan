8308c3f614edc94517ce372d0e0b3927be5c7843
hughes <hughes@tumelo.com>
2020-03-21 18:27:57 +0000

    Added authentication system

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,15 +1,26 @@
 import * as React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Accounts } from 'meteor/accounts-base'
 
 import ResolutionForm from './ResolutionForm'
 import ResolutionItem from './ResolutionItem'
+import RegisterForm from './RegisterForm'
+import LoginForm from './LoginForm'
 
 const App = ({ loading, refetch, resolutions }) => {
   if (loading) return null
 
   return (
     <>
+      {Accounts.userId() ? (
+        <button onClick={() => Meteor.logout()}>Logout</button>
+      ) : (
+        <>
+          <RegisterForm />
+          <LoginForm />
+        </>
+      )}
       <h1>Hello World</h1>
       <ResolutionForm refetch={refetch} />
       {resolutions.map(({ name, _id }) => (
